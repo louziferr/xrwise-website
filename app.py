b80c029110ef9860a0139fbbbf0d7d4e30291e4e
luzieahrens <luzie.ahrens@gmail.com>
2026-03-25 08:13:49 +0100

    add teams page

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def home():
     return render_template("xrwise_landingpage.html")
 
+@app.route("/team")
+def team():
+    return render_template("xrwise_team.html")
+
 if __name__ == "__main__":
     # Only used for local development
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
