7b3c678eb7f3f8a19096fa8ecae188349cb6804c
luzieahrens <luzie.ahrens@gmail.com>
2026-03-31 10:17:00 +0200

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~/home/dev/xrwise-website || exit
+
+git pull
+
+docker build -t flask-app .
+
+docker stop flask-app 2>/dev/null
+docker rm flask-app 2>/dev/null
+
+docker run -d -p 8081:5000 --name flask-app flask-app
